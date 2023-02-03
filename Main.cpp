c4a51b256cec0825071c4a8a73bc4e6b18825ec4
Ki Hun Sung <staragnarok@gmail.com>
2023-02-03 10:29:13 +0900

    skip initial inputs

--- Main.cpp
+++ Main.cpp
@@ -10,17 +10,17 @@ int main(int argc, char* argv[]) {
 
 	int thread_count = 8;
 	if (argc >= 2) thread_count = atoi(argv[1]);
-	else {
-		std::cout << "worker thread count (-1 = disable thread pool) : ";
-		std::cin >> thread_count;
-	}
+	//else {
+	//	std::cout << "worker thread count (-1 = disable thread pool) : ";
+	//	std::cin >> thread_count;
+	//}
 
 	int max_queue_size = -1;
 	if (argc >= 3) max_queue_size = atoi(argv[2]);	
-	else {
-		std::cout << "worker queue size (-1 = inf) : ";
-		std::cin >> max_queue_size;
-	}
+	//else {
+	//	std::cout << "worker queue size (-1 = inf) : ";
+	//	std::cin >> max_queue_size;
+	//}
 
 	std::string log_file_name = "none";
 	if (argc >= 4) log_file_name.assign(argv[3], strlen(argv[3]));
@@ -31,10 +31,10 @@ int main(int argc, char* argv[]) {
 
 	int test_case_count = 1000000;
 	if (argc >= 5) test_case_count = atoi(argv[4]);
-	else {
-		std::cout << "test_case_count  : ";
-		std::cin >> test_case_count;
-	}
+	//else {
+	//	std::cout << "test_case_count  : ";
+	//	std::cin >> test_case_count;
+	//}
 
 	int output_thread_count = 1;
 	if (log_file_name == "none")
